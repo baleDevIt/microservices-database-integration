af9ccb7ccc32d9d5a7864305fce16d263ea79927
Gennaro <bale.devit@gmail.com>
2022-12-15 07:27:07 +0100

    Init Master Microservices

---
+++ microservizioMaster/src/test/java/it/gbale/microservizioMaster/MicroservizioMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gbale.microservizioMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservizioMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
