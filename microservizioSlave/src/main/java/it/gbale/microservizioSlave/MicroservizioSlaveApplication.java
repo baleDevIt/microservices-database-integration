a6557c156619749bf1a73a1c063c5c983587058a
Gennaro <bale.devit@gmail.com>
2022-12-15 07:29:34 +0100

    Init Slave Microservices

---
+++ microservizioSlave/src/main/java/it/gbale/microservizioSlave/MicroservizioSlaveApplication.java
@@ -0,0 +1,13 @@
+package it.gbale.microservizioSlave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservizioSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservizioSlaveApplication.class, args);
+	}
+
+}
---
+++ microservizioSlave/src/test/java/it/gbale/microservizioSlave/MicroservizioSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gbale.microservizioSlave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservizioSlaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
